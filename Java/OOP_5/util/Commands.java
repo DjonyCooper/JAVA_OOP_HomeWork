ad442513a9a4d755d51fd594e93c288cbf4803c5
Djony Cooper <shammpenyon@gmail.com>
2023-03-22 17:53:58 +0300

    complete homework 5

---
+++ Java/OOP_5/util/Commands.java
@@ -0,0 +1,11 @@
+package Java.OOP_5.util;
+
+public enum Commands {
+    NONE,
+    READ,
+    CREATE,
+    UPDATE,
+    LIST,
+    DELETE,
+    EXIT
+}
---
+++ Java/OOP_5/view/WebView.java
@@ -0,0 +1,5 @@
+package Java.OOP_5.view;
+
+public class WebView {
+
+}
